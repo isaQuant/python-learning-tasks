4836a6a0a7de86c589094e9db5a0bc5db9fcee8d
isaQuant <isabella.quantschnig@gmx.net>
2021-10-23 20:59:33 +0200

    Added string example

---
+++ scr/Uni/string_example.py
@@ -0,0 +1,28 @@
+if __name__ == '__main__':
+
+    # Concenate two Strings
+    str1 = 'Hello'
+    str2 = 'World'
+    print(str1 + ' ' + str2)
+
+    # Access to individual character of a String
+    print(str1[0])
+    print(str1[2])
+
+    # Check if a String contains a specific Substring
+    # Returns true or false
+    print('He' in str1)
+
+    # Length of a String
+    print(len(str1))
+
+    # Variables
+    str1 = str2 = 'Hi'
+    print(str1)
+    print(str2)
+
+    # Some other examples
+    a, b = 6.0/4.0, 'Hello' + 'World'
+    print(a)
+    print(b)
+
